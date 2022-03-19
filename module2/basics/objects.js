b17ff58c07e9a75d9f1a24f35103e7549fc2a867
notharshprajapati <prajapati.harsh0243@gmail.com>
2022-03-19 14:19:01 +0530

    object in js

--- module2/basics/objects.js
+++ module2/basics/objects.js
@@ -1,2 +1,13 @@
-let obj = {};
-console.log(obj);
+let spooderman = {
+    name: 'Peter',
+    age: '19',
+    friends: ['mj', 'ned'],
+    address: {
+        city: 'queens',
+        state: 'ny'
+    },
+    sayhi: function () {
+        console.log('Cap is saying hello')
+    }
+};
+console.log(spooderman);
