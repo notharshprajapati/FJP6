baef76dc9d54c76392f6542a85323c3ee7846129
notharshprajapati <prajapati.harsh0243@gmail.com>
2022-03-19 18:14:07 +0530

    idk

---
+++ module2/introToNode/path.js
@@ -0,0 +1,10 @@
+let path = require("path");
+
+// let baseName = path.basename(__filename);
+// console.log(baseName);
+
+let dirPath = __dirname;
+console.log(dirPath);
+
+let newFilePath = path.join(dirPath,"lol.xd");
+console.log(newFilePath);
