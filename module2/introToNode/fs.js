71e612744e8a0d14e468c7290e0da64190f0ea29
notharshprajapati <prajapati.harsh0243@gmail.com>
2022-03-21 01:14:04 +0530

    21mar

--- module2/introToNode/fs.js
+++ module2/introToNode/fs.js
@@ -1,23 +1,39 @@
 let fs = require("fs");
-// console.log(fs);
 let path = require("path");
 
-let filePath = path.join(__dirname, "file.txt");
-// // console.log(filePath);
+// let filePath = path.join(__dirname, "file.txt");
 
-// //C-create
-//it creates file if it does not exists else it override
-fs.writeFileSync(filePath, "Hello I am a text file ");
+// // //C-create
+// //it creates file if it does not exists else it override
+// fs.writeFileSync(filePath, "Hello I am a text file ");
 
-// //R-read
-console.log("Before update : ");
-let content = fs.readFileSync(filePath, 'utf-8');
-console.log(content);
+// // //R-read
+// console.log("Before update : ");
+// let content = fs.readFileSync(filePath, 'utf-8');
+// console.log(content);
 
-// //U-update
-fs.appendFileSync(filePath, "\nNewly added content");
-console.log("After update : ")
-console.log(fs.readFileSync(filePath, 'utf-8'));
+// // //U-update
+// fs.appendFileSync(filePath, "\nNewly added content");
+// console.log("After update : ")
+// console.log(fs.readFileSync(filePath, 'utf-8'));
 
 // //D-delete
 // fs.unlinkSync(filePath);
+
+//check / make / delete dir
+if (!fs.existsSync("temp")) {
+    fs.mkdirSync("temp")
+}
+else {
+    fs.rmdirSync("temp")
+}
+
+// read dir
+let folderPath = "/media/harsh/Code/NADOS/FJP6/module2";
+let contentOfFolder = fs.readdirSync(folderPath);
+console.log(contentOfFolder);
+
+// //Copy
+// let copyContent = fs.readFileSync(filePath, 'utf-8');
+// let copyfilePath = path.join(__dirname, "copy.txt");
+// fs.writeFileSync(copyfilePath, copyContent);
