4cb07590650d017cf8b67f261e3d5548a599f598
notharshprajapati <prajapati.harsh0243@gmail.com>
2022-04-24 02:33:21 +0530

    idk

--- module2/automation/raw.js
+++ module2/automation/raw.js
@@ -2,7 +2,7 @@
 const puppeteer = require('puppeteer');
 
 let browserPromise = puppeteer.launch({ headless: false });
-
+ 
 browserPromise.then(function (browser) {
     console.log("Browser is opened");
     let pagePromise = browser.newPage();
