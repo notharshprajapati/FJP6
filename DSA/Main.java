933f0337875980f81f274d5e556a3d1261195228
notharshprajapati <prajapati.harsh0243@gmail.com>
2022-04-28 14:53:03 +0530

    module 3 started

--- DSA/Main.java
+++ DSA/Main.java
@@ -5,29 +5,17 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        ArrayList<String> list = getStairPaths(n);
+        String num1 = "19";
+        String num2 = "123";
+        int sum = 0;
+        int s1 = num1.length(), s2 = num2.length();
+        int diff = s2 - s1;
 
-        list.remove(list.size() - 1);
-        System.out.println(list);
-    }
-
-    public static ArrayList<String> getStairPaths(int n) {
-        if (n == 0) {
-            ArrayList<String> base = new ArrayList();
-            base.add("");
-            return base;
+        for (int i = 0; i < s2; i++) {
+            System.out.println(1);
         }
 
-        ArrayList<String> mylist = new ArrayList();
-        for (int i = 1; i <= n; i++) {
-            ArrayList<String> pathLen = getStairPaths(n - i);
-
-            for (String path : pathLen) {
-                mylist.add(i + path);
-            }
-        }
-        return mylist;
+        String str = num1 + num2;
+        System.out.println(str);
     }
-
 }
